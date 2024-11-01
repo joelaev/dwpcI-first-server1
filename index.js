c6e92cf71f2c25d27f2a41e0f4833edf4d11234c
Joel Escobar <160166118+joelaev@users.noreply.github.com>
2024-11-01 16:54:07 +0000

    Tercer check point: se ha creado el servidor

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 // 1. Importar el modulo http 
 import http from "http";
-
 // 2. Crear el servidor 
 // cb (callback) es una *funcion* que se ejecutara 
 // ante cualquier peticion de un recurso a nuestro server 
 // (request, response) 
 
 const server = http.createServer((req, res) => {
-    console.log("> Se ha recibido una petición."); 
-    // Respondemos al cliente
-    res.write("Hola");
-      // Se termina la conexión
-      res.end();
-  }); 
-
-// 3. Se pone a trabajar el servidor 
-// Se le pasa un callback que se ejecutara cunado
-// el servidor empiece a recibir peticiones 
-
-server.listen(3000, "0.0.0.0", () => {
-  console.log("👩‍🍳 Servidor escuchando en http://localhost:3000"); 
+    console.log(`📣 url: ${req.url}`);
+    console.log(`📣 method: ${req.method}`);
+    // Estableciendo cabeceras
+    res.setHeader('Content-Type', 'text/html');
+    // Escribiendo la respuesta
+    res.write('<html>');
+    res.write('<head> <title>My App</title></head>');
+    res.write('<body> <h1>Hello from my server</h1></body>');
+    res.write('</html>');
+    // Cerrando la comunicacion
+    res.end();
+  });
+  
+  // 3. Se pone a trabajar el servidor 
+  // Se le pasa un callback que se ejecutara cunado
+  // el servidor empiece a recibir peticiones 
+  
+  server.listen(3000, "0.0.0.0", () => {
+    console.log("👩‍🍳 Servidor escuchando en http://localhost:3000");
 });
